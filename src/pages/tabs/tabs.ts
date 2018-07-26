555b88ba6c1bbbabc60e77f07d8d3444b32dab3b
Developer <minorumadam@hotmail.com>
2018-07-26 10:51:43 +0100

    removed doc-a and doc b

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -8,8 +8,6 @@ import { IonicPage } from "ionic-angular";
 export class TabsPage {
   tab1Root = "LoginPage";
   tab2Root = "MainPage";
-  tab3Root = "DocAPage";
-  tab4Root = "DocBPage";
 
   constructor() {}
 }
